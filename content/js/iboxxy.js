6adb73e270a0359871c67700c5ccdea93fc1ec9e
axel669 <haryuu.hanekata@gmail.com>
2014-09-05 15:54:57 -0700

    changes

--- content/js/iboxxy.js
+++ content/js/iboxxy.js
@@ -14,10 +14,15 @@ function showAlert(msg)
 
 var overlay=createElem("div","iboxxy-overlay");
 var displayWrapper=createElem("div","iboxxy-display-wrapper");
+
+var titleWrapper=createElem("div");
+var contentWrapper=createElem("div","iboxxy-content-wrapper");
 var buttonWrapper=createElem("div","iboxxy-button-wrapper");
 
 buttonWrapper.innerHTML="<button class='iboxxy-button'>Wat</button>";
 
+displayWrapper.appendChild(titleWrapper);
+displayWrapper.appendChild(contentWrapper);
 displayWrapper.appendChild(buttonWrapper);
 overlay.appendChild(displayWrapper);
 
